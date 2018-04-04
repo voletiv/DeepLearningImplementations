192964d88fe4ffced7081e8c2a50be2ba602a86b
Vikram Voleti <vikram.voleti@gmail.com>
2018-04-04 09:25:09 +0000

    To use data augmentation or not

--- pix2pix/src/model/main.py
+++ pix2pix/src/model/main.py
@@ -20,14 +20,14 @@ def parse_my_args(patch_size=[64, 64], backend='tensorflow', generator_type='ups
     parser.add_argument('--nb_epoch', default=400, type=int, help="Number of batches per epoch")
     parser.add_argument('--n_run_of_gen_for_1_run_of_disc', default=1, type=int, help="After training disc on 1 batch, how many batches should gen train on")
     parser.add_argument('--save_weights_every_n_epochs', default=10, type=int, help="Epoch at which weights will be saved")
-    parser.add_argument('--visualize_images_every_n_epochs', default=1, type=int, help="Epoch at which images and graphs will be visualized")
+    parser.add_argument('--visualize_images_every_n_epochs', default=10, type=int, help="Epoch at which images and graphs will be visualized")
     parser.add_argument('--use_mbd', action="store_true", help="Whether to use minibatch discrimination")
     parser.add_argument('--use_label_smoothing', action="store_true", help="Whether to smooth the positive labels when training D")
     parser.add_argument('--label_flipping_prob', default=0, type=float, help="Probability (0 to 1.) to flip the labels when training D")
     parser.add_argument('--use_l1_weighted_loss', action="store_true", help="Whether to use l1 loss additionally weighted by mouth position (def: False)")
     parser.add_argument('--prev_model', default=None, type=str, help="model_name of previous model to load latest weights of")
     parser.add_argument('--discriminator_optimizer', default='sgd', type=str, help="discriminator_optimizer: sgd or (default) adam")
-    parser.add_argument('--MAX_FRAMES_PER_GIF', default=1000, type=int, help="Max number of frames to be saved in each gif")
+    parser.add_argument('--MAX_FRAMES_PER_GIF', default=100, type=int, help="Max number of frames to be saved in each gif")
     return parser.parse_args([str(patch_size[0]), str(patch_size[1]),
                               '--backend', backend,
                               '--generator_type', generator_type,
@@ -50,8 +50,9 @@ if __name__ == "__main__":
     parser.add_argument('--batch_size', default=4, type=int, help='Batch size')
     parser.add_argument('--n_batch_per_epoch', default=100, type=int, help="Number of training epochs")
     parser.add_argument('--nb_epoch', default=400, type=int, help="Number of batches per epoch")
+    parser.add_argument('--dont_augment_data', action="store_false", help="Use data augmentation while training")
     parser.add_argument('--save_weights_every_n_epochs', default=10, type=int, help="Epoch at which weights will be saved")
-    parser.add_argument('--visualize_images_every_n_epochs', default=1, type=int, help="Epoch at which images and graphs will be visualized")
+    parser.add_argument('--visualize_images_every_n_epochs', default=10, type=int, help="Epoch at which images and graphs will be visualized")
     parser.add_argument('--use_mbd', action="store_true", help="Whether to use minibatch discrimination")
     parser.add_argument('--use_label_smoothing', action="store_true", help="Whether to smooth the positive labels when training D")
     parser.add_argument('--label_flipping_prob', default=0, type=float, help="Probability (0 to 1.) to flip the labels when training D")
@@ -59,7 +60,7 @@ if __name__ == "__main__":
     parser.add_argument('--prev_model', default=None, type=str, help="model_name of previous model to load latest weights of")
     parser.add_argument('--discriminator_optimizer', default='sgd', type=str, help="discriminator_optimizer: sgd or (default) adam")
     parser.add_argument('--n_run_of_gen_for_1_run_of_disc', default=1, type=int, help="After training disc on 1 batch, how many batches should gen train on")
-    parser.add_argument('--MAX_FRAMES_PER_GIF', default=1000, type=int, help="Max number of frames to be saved in each gif")
+    parser.add_argument('--MAX_FRAMES_PER_GIF', default=100, type=int, help="Max number of frames to be saved in each gif")
 
     args = parser.parse_args()
     print(args)
@@ -96,6 +97,7 @@ if __name__ == "__main__":
                 "batch_size": args.batch_size,
                 "n_batch_per_epoch": args.n_batch_per_epoch,
                 "nb_epoch": args.nb_epoch,
+                "augment_data": (not args.dont_augment_data),
                 "model_name": model_name,
                 "save_weights_every_n_epochs": args.save_weights_every_n_epochs,
                 "visualize_images_every_n_epochs": args.visualize_images_every_n_epochs,
